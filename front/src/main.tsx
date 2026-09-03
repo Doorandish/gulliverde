562c04e67e001f5849437afa7e3273c28d4a163a
Amir Doorandish <amir-doorandish@Amirs-MacBook-Air.local>
2026-09-03 22:46:54 +0200

    fix(security): whitelist unsplash in CSP and disable buggy sw registration

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -5,11 +5,6 @@ import App from './App'
 import { LanguageProvider } from './hooks/useLanguage'
 import './index.css'
 
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker.register('/sw.js').catch(() => {})
-//   })
-// }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
